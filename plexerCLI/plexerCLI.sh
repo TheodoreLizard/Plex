b480cfbaea1e8626538ad817f5381e91cac6ba6b
Mark Steele <marksteele@mac.com>
2018-03-14 14:07:18 -0600

    Updated playlists

--- plexerCLI/plexerCLI.sh
+++ plexerCLI/plexerCLI.sh
@@ -2,5 +2,9 @@
 
 # IP Address of Plex server.
 export PLEX_ADDR=10.0.1.8
+export PLEX_TOKEN=Vq71cM1Lewqa6RRDB6UV
+
+# export PLEX_ADDR=10.0.1.7
+# export PLEX_TOKEN=swzgnJxeztjyes5mJ5EY
 
 ./plexerCLI playlist $@
